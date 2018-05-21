acd8ebabdaea3c192e84ad0d875d3a1113807e86
hiroaki.nishi <hiroaki.nish@gu3.co.jp>
2018-05-21 11:11:21 +0900

    1から5までを出力する。初期開発

---
+++ counter.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+
+def print_number():
+    """
+    1〜5までの数字を表示する
+    """
+    print 1
+    print 2
+    print 3
+    print 4
+    print 5
+
+
+# 数字を表示する
+print_number()
