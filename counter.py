7ba3a1014f7d5728195d01c82cc24bf84126037b
hiroaki.nishi <hiroaki.nish@gu3.co.jp>
2018-06-06 18:32:28 +0900

    数字表示関数をsubmodule化

--- counter.py
+++ counter.py
@@ -2,22 +2,7 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-
-def print_number(upper_count):
-    """
-    1から引数の値まで数字を表示する
-
-    :type upper_count: int
-    """
-    if not upper_count:
-        raise ValueError
-
-    print 'start.'
-
-    for i in xrange(upper_count):
-        print i + 1
-
-    print 'end.'
+from utils import print_number
 
 
 # 1から10までの数字を表示する
